a986282f4873bfd7634192d6a1914d06f1c4734e
Tom4s-L <tito_tom@hotmail.fr>
2021-05-20 21:40:09 +0200

    Design formulaire envoie message OK

--- mlp/src/Form/PostType.php
+++ mlp/src/Form/PostType.php
@@ -20,15 +20,15 @@ class PostType extends AbstractType
 
                 'label_attr' => ['class' => 'leading-7 text-md text-white'],
 
-                'attr' => ['class' => 'w-full bg-gray-100 bg-opacity-100 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200  h-11 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out']
+                'attr' => ['class' => 'bg-gray-100 bg-opacity-100 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200  h-11 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out mb-3']
             ])
 
             ->add('receiver', null, [
-                'label' => 'Choisissez un destinataire',
+                'label' => 'Destinataire',
 
-                'label_attr' => ['class' => 'leading-7 text-md text-white'],
+                'label_attr' => ['class' => 'leading-7 text-md text-white '],
 
-                'attr' => ['class' => 'w-full bg-gray-100 bg-opacity-100 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-11 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out'],
+                'attr' => ['class' => 'bg-gray-100 bg-opacity-100 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-11 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out mb-3'],
 
                 'placeholder' => 'Laissez vide si pas de destinataire en particulier',
             ])
@@ -38,12 +38,12 @@ class PostType extends AbstractType
 
                 'label_attr' => ['class' => 'leading-7 text-md text-white'],
 
-                'attr' => ['class' => 'w-full bg-gray-100 bg-opacity-100 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out'],
+                'attr' => ['class' => 'w-full bg-gray-100 bg-opacity-100 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-60 text-base outline-none text-gray-700 py-1 px-3 leading-6 transition-colors duration-200 ease-in-out '],
             ])
 
             ->add('submit', SubmitType::class, [
                 'label' => 'Enregistrer',
-                'attr' => ['class' => 'flex mx-auto text-white bg-green border-0 mt-6 py-2 px-8 focus:outline-none hover:bg-green-900 rounded text-lg'], 
+                'attr' => ['class' => 'flex mx-auto text-white bg-green border-0 mt-16 py-2 px-8 focus:outline-none hover:bg-green-900 rounded text-lg'], 
             ]
             )
         ;
