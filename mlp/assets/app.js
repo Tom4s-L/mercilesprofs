57167126761187bb8c4539b1fdcc05d376ebdaa3
Tom4s-L <tito_tom@hotmail.fr>
2021-05-27 16:35:29 +0200

    Print to PDF OK sans background

--- mlp/assets/app.js
+++ mlp/assets/app.js
@@ -36,10 +36,11 @@ const app = {
 
     printToPdf : function(event) {
         event.preventDefault();
+        console.log('ok');
         
         var printContents = document.querySelector('#posts').innerHTML;    
         var originalContents = document.body.innerHTML;  
-            
+
         document.body.innerHTML = printContents;     
         window.print();     
         document.body.innerHTML = originalContents;
